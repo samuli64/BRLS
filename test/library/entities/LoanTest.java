05740c5a416e99d75562f01c18d76cfa90e59b36
samuli64 <samuli64@users.noreply.github.com>
2020-09-29 09:49:22 +1000

    Create LoanTest.java

---
+++ test/library/entities/LoanTest.java
@@ -0,0 +1,34 @@
+package library.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoanTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCommit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckOverDue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsOverDue() {
+		fail("Not yet implemented");
+	}
+
+}
