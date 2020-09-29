0a5599dbd8f42dc5398ea5a7b35402c47c732cb3
samuli64 <samuli64@users.noreply.github.com>
2020-09-29 10:47:19 +1000

    Convert to Maven Project

--- test/library/entities/LoanTest.java
+++ test/library/entities/LoanTest.java
@@ -5,11 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+
+
+@ExtendWith(MockitoExtension.class)
 class LoanTest {
+	ILoan loan;
+	//@Mock IBook book;
+	//@Mock IPatron patron;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@AfterEach
