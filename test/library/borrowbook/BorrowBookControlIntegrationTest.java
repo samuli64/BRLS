67d81f0a2a3fd05ea96107944858ffd9259ac562
samuli64 <samuli64@users.noreply.github.com>
2020-10-03 19:36:38 +1000

    Create BorrowBookControlIntegrationTest.java

---
+++ test/library/borrowbook/BorrowBookControlIntegrationTest.java
@@ -0,0 +1,34 @@
+package library.borrowbook;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BorrowBookControlIntegrationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCardSwiped() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBookScanned() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCommitLoans() {
+		fail("Not yet implemented");
+	}
+
+}
