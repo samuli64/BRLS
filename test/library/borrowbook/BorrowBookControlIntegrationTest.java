8f7f83eca88be9d5a5a098d7e7176bebaaa77fce
samuli64 <samuli64@users.noreply.github.com>
2020-10-03 19:55:00 +1000

    Set setup method

--- test/library/borrowbook/BorrowBookControlIntegrationTest.java
+++ test/library/borrowbook/BorrowBookControlIntegrationTest.java
@@ -5,11 +5,40 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import library.entities.*;
+import library.entities.helpers.*;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 class BorrowBookControlIntegrationTest {
+	
+	@Mock IBorrowBookUI borrowBookUI;
+	IBorrowBookControl borrowBookControl;
+	
+	ILibrary library;
+	IPatron patron;
+	IBook bookTheHobbit;
+	IBook bookToKillAMockingbird;
+	IBook bookMobyDick;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		library = new Library(new BookHelper(), new PatronHelper(), new LoanHelper());
+		
+		bookTheHobbit = library.addBook("J.R.R. Tolkien", "The Hobbit", "c1");
+		bookToKillAMockingbird = library.addBook("Harper lee", "To Kill A Mockingbird", "c2");
+		bookMobyDick = library.addBook("Herman Melville", "Moby Dick", "c3");
+		
+		patron = library.addPatron("Mustermann", "Max", "max.mustermann@example.con", 198765432);
+		
+		borrowBookControl = new BorrowBookControl(library);
+		borrowBookControl.setUI(borrowBookUI);
 	}
 
 	@AfterEach
@@ -17,7 +46,7 @@ class BorrowBookControlIntegrationTest {
 	}
 
 	@Test
-	void testCardSwiped() {
+	void cardSwiped_ValidPatronId_SetsCurrentPatron() {
 		fail("Not yet implemented");
 	}
 
