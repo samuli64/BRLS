c640eb15ae3a454ecc1617011f597a7eeebbf7cc
samuli64 <samuli64@users.noreply.github.com>
2020-10-04 14:55:24 +1100

    Add cardSwiped and bookScanned integration tests

--- test/library/borrowbook/BorrowBookControlIntegrationTest.java
+++ test/library/borrowbook/BorrowBookControlIntegrationTest.java
@@ -7,12 +7,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import library.entities.*;
 import library.entities.helpers.*;
+import library.test.TestUtilities;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
@@ -26,6 +35,10 @@ class BorrowBookControlIntegrationTest {
 	IBook bookTheHobbit;
 	IBook bookToKillAMockingbird;
 	IBook bookMobyDick;
+	Date earlierDate;
+	
+	@Captor
+	ArgumentCaptor<String> displayStrings;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -39,6 +52,8 @@ class BorrowBookControlIntegrationTest {
 		
 		borrowBookControl = new BorrowBookControl(library);
 		borrowBookControl.setUI(borrowBookUI);
+		
+		earlierDate = TestUtilities.dateOf(2020, 2, 23);
 	}
 
 	@AfterEach
@@ -47,16 +62,313 @@ class BorrowBookControlIntegrationTest {
 
 	@Test
 	void cardSwiped_ValidPatronId_SetsCurrentPatron() {
-		fail("Not yet implemented");
+		// arrange
+		int patronId = patron.getId();
+		// act
+		borrowBookControl.cardSwiped(patronId);
+		IPatron actual = ((BorrowBookControl) borrowBookControl).getCurrentPatron();
+		// assert
+		assertEquals(patron, actual);
 	}
 
 	@Test
-	void testBookScanned() {
-		fail("Not yet implemented");
+	void cardSwiped_ValidPatronCanBorrow_CallsSetScanning() {
+		// arrange
+		int patronId = patron.getId();
+		// act
+		borrowBookControl.cardSwiped(patronId);
+		// assert
+		verify(borrowBookUI).setScanning();
+	}
+
+	@Test
+	void cardSwiped_ValidPatronCanBorrow_SetsStateScanning() {
+		// arrange
+		int patronId = patron.getId();
+		IBorrowBookControl.BorrowControlState expected = IBorrowBookControl.BorrowControlState.SCANNING;
+		// act
+		borrowBookControl.cardSwiped(patronId);
+		IBorrowBookControl.BorrowControlState actual = ((BorrowBookControl) borrowBookControl).getState();
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void cardSwiped_ControlStateNotSwiping_ThrowsException() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl = new BorrowBookControl(library);
+		// act
+		RuntimeException thrown = assertThrows(RuntimeException.class, 
+				() -> { borrowBookControl.cardSwiped(patronId); });
+		// assert
+		assertEquals(RuntimeException.class, thrown.getClass());
+	}
+	
+	@Test
+	void cardSwiped_InvalidPatronId_CallsDisplay() {
+		// arrange
+		int invalidPatronId = patron.getId() + 1;
+		String expectedMessage = "Invalid patronId";
+		// act
+		borrowBookControl.cardSwiped(invalidPatronId);
+		// assert
+		verify(borrowBookUI).display(expectedMessage);
+	}
+
+	@Test
+	void cardSwiped_InvalidPatronId_LeavesStateSwiping() {
+		// arrange
+		int invalidPatronId = patron.getId() + 1;
+		IBorrowBookControl.BorrowControlState expected = IBorrowBookControl.BorrowControlState.SWIPING;
+		// act
+		borrowBookControl.cardSwiped(invalidPatronId);
+		IBorrowBookControl.BorrowControlState actual = ((BorrowBookControl) borrowBookControl).getState();
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void cardSwiped_ValidPatronHasMaxFinesCantBorrow_CallsSetRestricted() {
+		// arrange
+		int patronId = patron.getId();
+		patron.incurFine(ILibrary.MAX_FINES_OWED);
+		// act
+		borrowBookControl.cardSwiped(patronId);
+		// assert
+		verify(borrowBookUI).setRestricted();
+	}
+
+	
+	@Test
+	void cardSwiped_ValidPatronHasMaxFinesCantBorrow_CallsDisplay() {
+		// arrange
+		int patronId = patron.getId();
+		patron.incurFine(ILibrary.MAX_FINES_OWED);
+		String expectedMessage = "Patron cannot borrow at this time";
+		// act
+		borrowBookControl.cardSwiped(patronId);
+		// assert
+		verify(borrowBookUI).display(expectedMessage);
+	}
+
+	
+	@Test
+	void cardSwiped_ValidPatronHasMaxFinesCantBorrow_SetsStateRestricted() {
+		// arrange
+		int patronId = patron.getId();
+		patron.incurFine(ILibrary.MAX_FINES_OWED);
+		IBorrowBookControl.BorrowControlState expected = IBorrowBookControl.BorrowControlState.RESTRICTED;
+		// act
+		borrowBookControl.cardSwiped(patronId);
+		IBorrowBookControl.BorrowControlState actual = ((BorrowBookControl) borrowBookControl).getState();
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void bookScanned_ValidBookIsAvailablePatronNotAtMaxLoans_DisplaysBookInformation() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+
+		// act
+		borrowBookControl.bookScanned(theHobbitId);
+		
+		// assert
+		verify(borrowBookUI).display(bookTheHobbit);
+	}
+
+	@Test
+	void bookScanned_ValidBookIsAvailablePatronNotAtMaxLoans_AddsNewPendingLoan() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+		List<ILoan> pendingLoans = ((BorrowBookControl) borrowBookControl).getPendingLoans();
+		int numberOfPendingLoans = pendingLoans.size(); 
+		assertEquals(0, numberOfPendingLoans);
+
+		// act
+		borrowBookControl.bookScanned(theHobbitId);
+		numberOfPendingLoans = pendingLoans.size();
+		ILoan pendingLoan = pendingLoans.get(0);
+		
+		// assert
+		assertEquals(1, numberOfPendingLoans);
+		assertEquals(bookTheHobbit, pendingLoan.getBook());
+	}
+	
+	@Test
+	void bookScanned_ValidBookIsAvailablePatronNotAtMaxLoans_StateRemainsScanning() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+		IBorrowBookControl.BorrowControlState state = ((BorrowBookControl) borrowBookControl).getState();
+		assertEquals(IBorrowBookControl.BorrowControlState.SCANNING, state);
+
+		// act
+		borrowBookControl.bookScanned(theHobbitId);
+		state = ((BorrowBookControl) borrowBookControl).getState();
+		
+		// assert
+		assertEquals(IBorrowBookControl.BorrowControlState.SCANNING, state);
+	}
+
+	@Test
+	void bookScanned_ValidBookIsAvailablePatronWillBeAtMaxLoans_DisplaysLoanLimitReached() {
+		// arrange
+		int patronId = patron.getId();
+		for (int loanId = 1; loanId < ILibrary.LOAN_LIMIT; loanId++) {
+			IBook tmpBook = new Book("author", "title", "callNumber1", loanId);
+			ILoan tmpLoan = new Loan(tmpBook, patron);
+			tmpLoan.commit(loanId, earlierDate);
+		}
+		assertEquals(ILibrary.LOAN_LIMIT - 1, patron.getNumberOfCurrentLoans());
+		
+		IBorrowBookControl spyBorrowBookControl = spy(borrowBookControl);
+		doNothing().when(spyBorrowBookControl).borrowingCompleted();
+		spyBorrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+		
+		String expected = "Loan limit reached";
+		
+		// act
+		spyBorrowBookControl.bookScanned(theHobbitId);
+		
+		// assert
+		verify(borrowBookUI, times(2)).display(displayStrings.capture());
+		assertEquals(bookTheHobbit, displayStrings.getAllValues().get(0));
+		assertEquals(expected, displayStrings.getAllValues().get(1));
+	}
+
+	@Test
+	void bookScanned_ValidBookIsAvailablePatronWillBeAtMaxLoans_AddsNewPendingLoan() {
+		// arrange
+		int patronId = patron.getId();
+		for (int loanId = 1; loanId < ILibrary.LOAN_LIMIT; loanId++) {
+			IBook tmpBook = new Book("author", "title", "callNumber1", loanId);
+			ILoan tmpLoan = new Loan(tmpBook, patron);
+			tmpLoan.commit(loanId, earlierDate);
+		}
+		assertEquals(ILibrary.LOAN_LIMIT - 1, patron.getNumberOfCurrentLoans());
+		
+		IBorrowBookControl spyBorrowBookControl = spy(borrowBookControl);
+		doNothing().when(spyBorrowBookControl).borrowingCompleted();
+		spyBorrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+		List<ILoan> pendingLoans = ((BorrowBookControl) spyBorrowBookControl).getPendingLoans();
+		int numberOfPendingLoans = pendingLoans.size(); 
+		assertEquals(0, numberOfPendingLoans);
+
+		// act
+		spyBorrowBookControl.bookScanned(theHobbitId);
+		numberOfPendingLoans = pendingLoans.size();
+		ILoan pendingLoan = pendingLoans.get(0);
+		
+		// assert
+		assertEquals(1, numberOfPendingLoans);
+		assertEquals(bookTheHobbit, pendingLoan.getBook());
+	}
+
+	@Test
+	void bookScanned_ValidBookIsAvailablePatronWillBeAtMaxLoans_CallsBorrowingCompleted() {
+		// arrange
+		int patronId = patron.getId();
+		for (int loanId = 1; loanId < ILibrary.LOAN_LIMIT; loanId++) {
+			IBook tmpBook = new Book("author", "title", "callNumber1", loanId);
+			ILoan tmpLoan = new Loan(tmpBook, patron);
+			tmpLoan.commit(loanId, earlierDate);
+		}
+		assertEquals(ILibrary.LOAN_LIMIT - 1, patron.getNumberOfCurrentLoans());
+		
+		IBorrowBookControl spyBorrowBookControl = spy(borrowBookControl);
+		doNothing().when(spyBorrowBookControl).borrowingCompleted();
+		spyBorrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+
+		// act
+		spyBorrowBookControl.bookScanned(theHobbitId);
+		
+		// assert
+		verify(spyBorrowBookControl).borrowingCompleted();
+	}
+
+	@Test
+	void bookScanned_InvalidBookId_DisplaysInvalidBookIdMessage() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl.cardSwiped(patronId);
+		int invalidId = -1;
+		String expected = "Invalid bookId";
+
+		// act
+		borrowBookControl.bookScanned(invalidId);
+		
+		// assert
+		verify(borrowBookUI).display(expected);
+	}
+
+	@Test
+	void bookScanned_InvalidBookId_StateRemainsScanning() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl.cardSwiped(patronId);
+		int invalidId = -1;
+		IBorrowBookControl.BorrowControlState state = ((BorrowBookControl) borrowBookControl).getState();
+		assertEquals(IBorrowBookControl.BorrowControlState.SCANNING, state);
+
+		// act
+		borrowBookControl.bookScanned(invalidId);
+		state = ((BorrowBookControl) borrowBookControl).getState();
+		
+		// assert
+		assertEquals(IBorrowBookControl.BorrowControlState.SCANNING, state);
+	}
+
+	@Test
+	void bookScanned_ValidBookIsUnavailable_DisplaysInvalidBookIdMessage() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+		bookTheHobbit.borrowFromLibrary();
+		String expected = "Book cannot be borrowed";
+
+		// act
+		borrowBookControl.bookScanned(theHobbitId);
+		
+		// assert
+		verify(borrowBookUI).display(expected);
+	}
+
+	@Test
+	void bookScanned_ValidBookIsUnavailable_StateRemainsScanning() {
+		// arrange
+		int patronId = patron.getId();
+		borrowBookControl.cardSwiped(patronId);
+		int theHobbitId = bookTheHobbit.getId();
+		bookTheHobbit.borrowFromLibrary();
+		IBorrowBookControl.BorrowControlState state = ((BorrowBookControl) borrowBookControl).getState();
+		assertEquals(IBorrowBookControl.BorrowControlState.SCANNING, state);
+
+		// act
+		borrowBookControl.bookScanned(theHobbitId);
+		state = ((BorrowBookControl) borrowBookControl).getState();
+		
+		// assert
+		assertEquals(IBorrowBookControl.BorrowControlState.SCANNING, state);
 	}
 
 	@Test
 	void testCommitLoans() {
+		// arrange
+		
+		// act
+		
+		// assert
 		fail("Not yet implemented");
 	}
 
